e3897ce3f81523eb0675b4a43207bbc2d0c7ad38
Yvonne <yvonne.broek@gmail.com>
2019-02-04 19:01:06 +0000

    TWD up to and including CH10, hopefully fixed failed test CH9

--- rango/views.py
+++ rango/views.py
@@ -8,15 +8,32 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
+from datetime import datetime
+
 def index(request):
+	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories':category_list, 'pages': pages_list}
-	# return render(request, reverse('index'), context=context_dict)
-	return render(request, 'rango/index.html', context=context_dict)
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
+	# Obtain our response object early so we can add cookie info
+	response = render(request, 'rango/index.html', context=context_dict)
+
+	# Return response back to the user, updating any cookies that needed change
+	return response
 
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
 	context_dict = {'name': "Yvonneke van den Broek"}
+
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
 	return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -190,7 +207,30 @@ def user_logout(request):
 	# Take the user back to the homepage
 	return HttpResponseRedirect(reverse('index'))
 
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
 
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	# If it's been more than a day since the last visit:
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+
+	# Update/set the visits cookie
+	request.session['visits'] = visits
 
 
 
